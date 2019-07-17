78b9580b094965cc41a1ff54494b0bcc1b72338a
Sam Estep <sam@samestep.com>
2019-07-17 09:22:37 -0400

    Update comments in Java file

--- infer/tests/codetoanalyze/java/gradual/Gradual.java
+++ infer/tests/codetoanalyze/java/gradual/Gradual.java
@@ -54,39 +54,39 @@ class Bar {
 class MatchInstrTests {
   static void assignChecksLhs() {
     Foo x = null;
-    x.f = null; // should warn about null deference
+    x.f = null; // should err about null deference
   }
 
   static void assignChecksRhs() {
     Foo x = null;
-    Object y = x.f; // should warn about null dereference
+    Object y = x.f; // should err about null dereference
   }
 
   static void assignMakesNonnull() {
     Foo x = Foo.mystery();
-    Object y1 = x.f; // should warn about possible null dereference
+    Object y1 = x.f; // should err about possible null dereference
     x = new Foo();
-    Object y2 = x.f; // shouldn't warn
+    Object y2 = x.f; // shouldn't err
   }
 
   static void assignChecksLhsFieldAnnot() {
     Bar x = new Bar();
-    x.s = null; // should warn about field annotation violation
+    x.s = null; // should err about field annotation violation
   }
 
   static void assignAllowsNonnullFieldAnnot() {
     Bar x = new Bar();
-    x.s = "henlo worl"; // shouldn't warn
+    x.s = "henlo worl"; // shouldn't err
   }
 
   static void assignAllowsNullableFieldAnnot() {
     Foo x = new Foo();
-    x.f = null; // shouldn't warn
+    x.f = null; // shouldn't err
   }
 
   static void assumeChecksCond() {
     Foo x = null;
-    if (x.f == null) { // should warn about null dereference
+    if (x.f == null) { // should err about null dereference
       x = null;
     }
   }
@@ -94,52 +94,52 @@ class MatchInstrTests {
   static void assumeMakesNonnull() {
     Foo x = Foo.mystery();
     if (x != null) {
-      Object y = x.f; // shouldn't warn
+      Object y = x.f; // shouldn't err
     }
   }
 
   static void callStaticAcceptsNullFirstArg() {
     Foo x = null;
-    Object y = Foo.takeNull(x); // shouldn't warn
+    Object y = Foo.takeNull(x); // shouldn't err
   }
 
   static void callStaticChecksAllAnnots() {
     Foo x = Foo.mystery();
     Foo y = Foo.mystery();
     Foo z = Foo.mystery();
-    Object o = Foo.firstF(x, y, z); // should warn about x and z
+    Object o = Foo.firstF(x, y, z); // should err about x and z
   }
 
   static void callMethodRejectsNullReceiver() {
     Foo x = null;
-    Object y = x.getF(); // should warn about null dereference
+    Object y = x.getF(); // should err about null dereference
   }
 
   static void callMethodChecksAllAnnots() {
     Foo x = new Foo();
     Foo y = Foo.mystery();
     Foo z = Foo.mystery();
-    Object o = x.firstF(y, z); // should warn about z
+    Object o = x.firstF(y, z); // should err about z
   }
 
   static void callMakesNonnull() {
     Bar x = new Bar();
     String s = x.getSNullable();
-    int l1 = s.length(); // should warn about possible null dereference
+    int l1 = s.length(); // should err about possible null dereference
     s = x.getS();
-    int l2 = s.length(); // shouldn't warn
+    int l2 = s.length(); // shouldn't err
   }
 
   static void fieldGivesNullableAnnot() {
     Foo x = new Foo();
     Object y = x.f;
-    int z = y.hashCode(); // should warn about null dereference
+    int z = y.hashCode(); // should err about null dereference
   }
 
   static void fieldGivesNonnullAnnot() {
     Bar x = new Bar();
     String s = x.s;
-    int l = s.length(); // shouldn't warn
+    int l = s.length(); // shouldn't err
   }
 
   static void arrayItemsQuestionMark() {
@@ -147,13 +147,13 @@ class MatchInstrTests {
     ss[0] = "Hello, world!";
     String a = ss[0];
     String b = ss[1];
-    int al = a.length(); // shouldn't warn
-    int bl = b.length(); // shouldn't warn
+    int al = a.length(); // should warn but not err
+    int bl = b.length(); // should warn but not err
   }
 
   static void castsAreStillNull() {
     Foo x = (Foo) null;
-    Object y = x.f; // should warn about null dereference
+    Object y = x.f; // should err about null dereference
   }
 
   static void logicChecksCompoundExprs() {
@@ -164,17 +164,17 @@ class MatchInstrTests {
     // looks like true case should say b is nonnull
     // but the compound cond expr is split b/c of short-circuiting
     if (!((a == null || b == null) && !(b == x && c != null))) {
-      Foo.complain(a, b, c); // should warn about a, b, c
+      Foo.complain(a, b, c); // should err about a, b, c
     } else {
-      Foo.complain(a, b, c); // should warn about a, b, c
+      Foo.complain(a, b, c); // should err about a, b, c
     }
   }
 
   static Object canReturnNullWithoutNonnull() {
-    return null; // shouldn't warn
+    return null; // shouldn't err
   }
 
   static @Nonnull Object cantReturnNullWithNonnull() {
-    return null; // should warn about null return
+    return null; // should err about null return
   }
 }
